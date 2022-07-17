7eb18583174c2360cc4242257713c166dae093da
bulbabyte <77218083+bulbabyte@users.noreply.github.com>
2022-07-17 16:24:47 -0500

    Update Weather.py

--- Python/Weather.py
+++ Python/Weather.py
@@ -5,3 +5,14 @@ if not (haveUmbrella or weatherIsNice):
     print("Stay inside")
 else:
     print("Go for a walk")
+
+# More readable version
+
+weatherIsNice = True
+haveUmbrella = False
+
+if haveUmbrella or weatherIsNice:
+    print('Go for a walk')
+else:
+    print('Stay inside')
+    
